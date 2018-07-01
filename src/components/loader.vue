c5b76a7f03477536ba72af5a585071294526cd9d
Lera <lera.varnitskaya@gmail.com>
2018-07-01 16:05:56 +0300

    added loader component

---
+++ src/components/loader.vue
@@ -0,0 +1,21 @@
+<template>
+    <span class="loader-wrapper" v-if="isLoading">
+      <img src="../assets/img/loader.gif" :width="size" :height="size">
+    </span>
+</template>
+
+<script>
+    export default {
+        name: "loader",
+        props: {
+          size: Number,
+          isLoading: Boolean
+        }
+    }
+</script>
+
+<style scoped>
+  .loader-wrapper img {
+    height: 100%;
+  }
+</style>
