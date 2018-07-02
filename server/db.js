3cea134de3b3cabda8828b3d581220bb2db5209f
tomash <vv.tomash@gmail.com>
2018-07-02 10:08:04 +0300

    .m

--- server/db.js
+++ server/db.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
-const init = require('./init.json');
 
 const { Schema } = mongoose;
 
@@ -9,28 +8,19 @@ const userSchema = new Schema({
   pwd: String,
 });
 
+userSchema.query.byName = function (name) {
+  return this.where({ name: new RegExp(name, 'i') });
+};
+
 const Models = {
   User: mongoose.model('User', userSchema),
-  initialized: false,
+  initialized: true,
 };
 
-const initialize = function () {
-  Models.User.find(null, (err, doc) => {
-    if (err) {
-      console.log(err);
-    } else if (!doc.length) {
-      console.log('Database opens for the first time...');
-      Promise.all(init.map(item => new Models[item.type](item).save()))
-        .then(() => console.log('Initialize successfully.'))
-        .catch(() => console.log('Something went wrong during initializing.'));
-    } else {
-      Models.initialized = true;
-    }
-  });
-};
-
-mongoose.connect('mongodb://127.0.0.1/CMS2');
-// mongoose.set('debug', true)
+mongoose.connect('mongodb://127.0.0.1/BM');
+if (process.env.ENV_NODE !== 'production') {
+  mongoose.set('debug', true);
+}
 
 const db = mongoose.connection;
 
@@ -40,7 +30,6 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('The database has connected.');
-  initialize();
 });
 
 module.exports = Models;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const winston = require('winston');
 const express = require('express');
-// const favicon = require('serve-favicon')
-// const bodyParser = require('body-parser')
-// const cookieParser = require('cookie-parser')
-// const db = require('./db')
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const api = require('./api');
 
 const logger = winston.createLogger({
@@ -16,18 +15,23 @@ const resolve = file => path.resolve(__dirname, file);
 
 const app = express();
 
-app.set('port', (process.env.port || 3000));
-// app.use(favicon(resolve('../dist/favicon.ico')))
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(cookieParser())
+app.set('port', (process.env.PORT || 3000));
+app.use(favicon(resolve('../dist/favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use('/dist', express.static(resolve('../dist')));
-app.use(api);
+app.use('/api', api);
 
 app.get('*', (req, res) => {
   const fileName = 'index.html';
   const html = fs.readFileSync(resolve(`../src/${fileName}`), 'utf-8');
   res.send(html);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
