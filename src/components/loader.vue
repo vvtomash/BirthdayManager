ba433c4aeb7cd81539008e283dcbc7284b759cb6
Lera <lera.varnitskaya@gmail.com>
2018-07-03 12:40:30 +0300

    addressed comments

--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,21 +1,102 @@
 <template>
-    <span class="loader-wrapper" v-if="isLoading">
-      <img src="../assets/img/loader.gif" :width="size" :height="size">
-    </span>
+  <div
+    v-if="isLoading"
+    class="overlay">
+    <div class="lds-ellipsis">
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "loader",
-        props: {
-          size: Number,
-          isLoading: Boolean
-        }
-    }
+export default {
+  name: 'Loader',
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .loader-wrapper img {
-    height: 100%;
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: black;
+    opacity: 0.5;
+  }
+  .lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 64px;
+    height: 64px;
+  }
+
+  .lds-ellipsis div {
+    position: absolute;
+    top: 27px;
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background: #fff;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+
+  .lds-ellipsis div:nth-child(1) {
+    left: 6px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+
+  .lds-ellipsis div:nth-child(2) {
+    left: 6px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+
+  .lds-ellipsis div:nth-child(3) {
+    left: 26px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+
+  .lds-ellipsis div:nth-child(4) {
+    left: 45px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(19px, 0);
+    }
   }
 </style>
